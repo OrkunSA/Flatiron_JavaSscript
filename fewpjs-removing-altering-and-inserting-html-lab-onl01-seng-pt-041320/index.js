490235eeb1ac87d5e434e1eccb09bed590712f8e
OrkunSA <c.orkun.s@gmail.com>
2020-10-12 21:35:31 -0500

    adding solution

--- fewpjs-removing-altering-and-inserting-html-lab-onl01-seng-pt-041320/index.js
+++ fewpjs-removing-altering-and-inserting-html-lab-onl01-seng-pt-041320/index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeadder = document.createElement('h1');
+newHeader.id = "victory";
+newHeadder.className = 'victory';
+newHeader.innerHTML = "Orkun is the champion";
