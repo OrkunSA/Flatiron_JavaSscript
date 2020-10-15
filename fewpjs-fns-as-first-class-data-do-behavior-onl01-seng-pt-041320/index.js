7a0d82ee082d8e3bdd49067a556906c0be074f88
OrkunSA <c.orkun.s@gmail.com>
2020-10-14 21:06:03 -0500

    adding labs

---
+++ fewpjs-fns-as-first-class-data-do-behavior-onl01-seng-pt-041320/index.js
@@ -0,0 +1,13 @@
+/* Given Code, don't edit */
+
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick(e) {
+  const timeString = document.getElementById('time').value
+  displayMessage(greet(timeString))
+}
+
+/* End Given Code, don't edit above here...*/
+
+/* Write your implementation of greet() */
+/* Write your implementation of displayMessage() */
