9fbaa2fd26ec2a8f7e03d5a367b9544321979df9
OrkunSA <c.orkun.s@gmail.com>
2020-11-07 14:14:33 -0600

    adding solution

--- fewpjs-iterators-fndcl-fnexpr-filter-lab-onl01-seng-pt-041320/index.js
+++ fewpjs-iterators-fndcl-fnexpr-filter-lab-onl01-seng-pt-041320/index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const findMatching = (drivers, string) => {
+    return drivers.filter(possibleMatch =>
+        possibleMatch.toLowerCase() === string.toLowerCase())
+}
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(possibleMatch =>
+        possibleMatch.startswith(string) === string.startswith(string))
+}
+
+const matchName = (drivers, string) => {
+    return drivers.filter(possibleMatch =>
+        possibleMatch.name === string)
+}
